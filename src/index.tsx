bebe370ba397d7b67c397ea35fe171b9b3234204
Eh1Ka6 <159229@supinfo.com>
2021-10-20 01:06:21 +0200

    p

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AppMint from './AppMint';
 
-
-
+const domContainers = document.querySelector('#appp');
+ReactDOM.render(<App></App>, domContainers);
 const domContainer = document.querySelector('#appMint');
 ReactDOM.render(<AppMint></AppMint>, domContainer);
 // If you want to start measuring performance in your app, pass a function
