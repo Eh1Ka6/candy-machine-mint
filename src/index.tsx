70aa1b2f369cbbc8a48552407650e1edcaad44d6
Eh1Ka6 <159229@supinfo.com>
2021-10-20 02:24:32 +0200

    k

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AppMint from './AppMint';
+
 
 const domContainers = document.querySelector('#appp');
 ReactDOM.render(<App></App>, domContainers);
-const domContainer = document.querySelector('#appMint');
-ReactDOM.render(<AppMint></AppMint>, domContainer);
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
